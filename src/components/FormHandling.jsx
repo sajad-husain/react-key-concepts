77dd01927ee68c764bc27bd6d8e8b93b71a8800e
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-02-25 12:01:37 +0500

    Form created

---
+++ src/components/FormHandling.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FormHandling = () => {
+    return (
+        <form>
+            <input type="text" />
+            <br />
+            <input type="text" />
+            <br />
+            <button>Login</button>
+        </form>
+    )
+}
+
+export default FormHandling
